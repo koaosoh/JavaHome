ecc4a4af3388589f8febbe0bef2701589e7f8fec
koaosoh <koaosoh@gmail>
2015-09-03 15:44:30 -0400

    Exercise4

---
+++ DayManvNightMan/src/DayTwo/OrderChecker.java
@@ -0,0 +1,15 @@
+package DayTwo;
+
+import java.util.Scanner;		
+
+public class OrderChecker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int b;
+		int n;
+		int w;
+	}
+
+}
